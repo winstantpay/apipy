a1ef56d0675cf76f93c999d996e5353dbbbdc710
winstantpay <ralf@winstantpay.com>
2018-05-29 09:20:28 +0700

    Minor code beautifications & typos

--- CustomerAccountBalancesGet.py
+++ CustomerAccountBalancesGet.py
@@ -11,7 +11,7 @@ def main():
     res = client.service.UserSettingsGetSingle(request = {
         'ServiceCallerIdentity':{
             'LoginId': config.userLogin,
-            'Password': config.gituserPassword,
+            'Password': config.userPassword,
             'ServiceCallerId': config.callerId
         }
     })
