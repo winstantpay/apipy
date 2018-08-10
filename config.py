6602a056739e966538795796caec7094f4a21185
winstantpay <ralf@winstantpay.com>
2018-08-10 16:46:19 +0700

    V2.8

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # Please get your login credentials at 
 # demo.winstantpay.com and register with WorldKyc
 #
-userLogin = "NewCustSolidus"
-userPassword = "HNmQfyzv3"
-callerId = "i73B3EBA-D4FC-4853-A32F-06FD23A5C902"
+userLogin = ""
+userPassword = ""
+callerId = ""
